c3089700a9ca43bf7f6dce4f9ece18597dd78bea
Jan-Oliver Kaiser <mail@janno-kaiser.de>
2012-06-04 11:49:26 +0200

    WIP MN

--- myhill_nerode.v
+++ myhill_nerode.v
@@ -10,7 +10,7 @@ Section MyhillNerode.
     Variable X: finType.
     Variable L: language char.
 
-    Definition MH_rel x y := forall z, x++z \in L <-> y++z \in L.
+    Definition MH_rel w1 w2 := forall w3, w1++w3 \in L <-> w2++w3 \in L.
 
     (* We model finiteness of equivalence classes
      by functions to some finType. *)
@@ -19,9 +19,23 @@ Section MyhillNerode.
     
     (* We define what it means to be a refinement
      of the MH relation: *)
-    Definition MH_rel_ref (f: Fin_eq_cls) := forall x y, f x = f y -> MH_rel x y.
+    Definition MH_rel_ref (f: Fin_eq_cls) := forall w1 w2, f w1 = f w2 -> MH_rel w1 w2.
     
   End Relation.
+
+  Section ToMN.
+    Variable A: dfa char.
+    Definition f : Fin_eq_cls A := [ fun w => last (dfa_s0 A) (dfa_run A w) ].
+
+    Lemma f_correct: MH_rel_ref _ (dfa_lang A) f.
+    Proof.
+      move => w1 w2.
+      rewrite /f /=.
+      move => H0 w3.
+      rewrite /dfa_lang /= 2!in_simpl /= -2!dfa_run_accepts 2!dfa_run'_cat 2!last_cat.
+      by rewrite H0.
+    Qed.
+  End ToMN.
   
   Section ToDFA.
     Variable L: language char.
@@ -29,103 +43,64 @@ Section MyhillNerode.
     Variable f: Fin_eq_cls X.
     Variable ref: MH_rel_ref X L f.
 
+    Definition surj (f: Fin_eq_cls X) := forall x, exists w, f(w) == x. 
 
-    
-    Fixpoint repr n: seq word :=
-      match n with
-      | 0 => [::[::]]
-      | n.+1 => flatten (map (fun w => map (fun a => rcons w a) (enum char)) (repr n))
-      end.
+    Variable f_surj: surj f.
 
-    Lemma mem_flatten (w: word) ww: w \in flatten ww -> { w' | w' \in ww /\ w \in w' }.
+    Definition f_inv := fun x => xchoose (f_surj x).
+
+    Definition f_invK: cancel f_inv f.
     Proof.
-      elim: ww => [|w' ww IHww] //.
-      rewrite /= mem_cat.
-      case H: (w \in w') => //=.
-        move => H0. exists w'.
-        by rewrite mem_head.
-      move/IHww => [] w'' [] H0 H1.
-      exists w''.
-      by rewrite in_cons H0 orbT.
+      move => x.
+      by move: (xchooseP (f_surj x)) => /eqP {2}<-.
     Qed.
 
-    Lemma mem_flatten' (w: word) w' (ww: seq (seq word)): w \in w' -> w' \in ww -> w \in flatten ww.
-    Proof.
-      elim: ww => [|w'' ww IHww] //.
-      rewrite /= in_cons => H0 /orP [/eqP <-|].
-        by rewrite mem_cat H0.
-      move/(IHww H0) => H1.
-      by rewrite mem_cat H1 orbT.
-    Qed.
-      
-    
-    Lemma repr_correct n w: w \in repr n <-> size w = n.
-    Proof.
-      elim: n w => [|n IHn] w.
-        split.
-          rewrite /= -topredE /=.
-          by move/orP => [] // /eqP ->.
-        by move/size0nil => ->.
-      split.
-        rewrite /= => /mem_flatten => [].
-        move => [] s [].
-        move/mapP => [] v.
-        move/IHn => H0 -> /mapP [] c _ ->.
-        by rewrite size_rcons H0.
-      case/lastP: w => [|w a] //.
-      rewrite size_rcons => H0.
-      inversion H0. move/IHn: (H1).
-      rewrite /= => H2.
-      apply: (mem_flatten' _ (map (fun a => rcons w a) (enum char))).
-        apply/mapP.
-        exists a => //.
-        by rewrite mem_enum.
-      apply/mapP.
-      exists w => //.
-      by rewrite H1.
-    Qed.
-
-    Definition repr_type n : finType := seq_sub_finType (repr n).
-
-    Lemma exists2_exists (Y: Type) (p q: Y -> Prop): (exists2 x: Y, p x & q x) <-> exists x: Y, p x /\ q x.
-    Proof.
-      split.
-        move => [] x H0 H1.
-        by exists x.
-      move => [] x [] H0 H1.
-      by exists x.
-    Qed.
-
-    
-    Definition repr_type_word n (w: repr_type n): word.
-    Proof.
-      inversion w.
-      exact ssval.
-    Defined.
-
-    Lemma repr_type_word_inj n: injective (repr_type_word n).
+    Definition repr := [ fun w => f_inv (f w) ].
+                                         
+    Lemma f_inv_inj: injective f_inv.
     Proof.
       move => x y.
-      rewrite /repr_type_word /=.
-      case: x. case: y.
-      move => x Hx y Hy H0.
+      move: (xchooseP (f_surj x)) => /eqP {2}<-.
+      move: (xchooseP (f_surj y)) => /eqP {2}<-.
+      by rewrite /f_inv => ->.
+    Qed.
       
+    Definition state : finType := X.
 
-     Lemma test n x y (Hy: y \in repr n) Hx: y = x -> 
-       {| ssval := y; ssvalP := Hy |} = {| ssval := x; ssvalP := Hx |}.
-     move => ->.
+    Definition s0 : state := f [::].
 
-      
-      
-      
-      
+    Definition fin: pred state :=
+      [pred x | f_inv x \in L ].
     
-    Lemma repr_type_correct n (w: repr_type n): size w = n.
+    Definition step x (a: char): X :=
+      f (rcons (f_inv x) a).
 
+    Definition MN_dfa := dfaI char state s0 fin step.
+
+    
+    Lemma MN_dfa_run_f w: last s0 (dfa_run' MN_dfa s0 w) = f w.
+    Proof.
+      move: w.
+      apply: last_ind => [|w a IHw] //.
+      rewrite -{1}cats1 dfa_run'_cat last_cat IHw /=.
+      rewrite /step.
       
-    Fixpoint canonical_repr n: finType :=
-      match n with
-        | 0 => Q
-                 
+      
+    Lemma MN_dfa_correct: L =1 dfa_lang MN_dfa.
+    Proof.
+      case/lastP.
+        have H1: f [::] = f (f_inv s0).
+          move: (xchooseP (f_surj s0)) => /eqP.
+          by rewrite /s0 /f_inv.
+        apply/idP/idP => /= H0.
+          rewrite /fin /=.
+          move: (ref _ _ H1 [::]).
+          rewrite 2!cats0.
+          case.
+          by auto.
+        move: (ref _ _ H1 [::]) => /=.
+        rewrite cats0. case => _ H2.
+        apply: H2.
+        
   End ToDFA.
 End MyhillNerode.
