c0421d9d87d620dec7441c69f76bea0528bc7749
Jan-Oliver Kaiser <mail@janno-kaiser.de>
2012-06-06 22:41:56 +0200

    switched to equivalence in MN_rel. MN -> dfa done.

--- myhill_nerode.v
+++ myhill_nerode.v
@@ -19,24 +19,10 @@ Section MyhillNerode.
     
     (* We define what it means to be a refinement
      of the MH relation: *)
-    Definition MH_rel_ref (f: Fin_eq_cls) := forall w1 w2, f w1 = f w2 -> MH_rel w1 w2.
+    Definition MH_rel_ref (f: Fin_eq_cls) := forall w1 w2, f w1 = f w2 <-> MH_rel w1 w2.
     
   End Relation.
 
-  Section ToMN.
-    Variable A: dfa char.
-    Definition f : Fin_eq_cls A := [ fun w => last (dfa_s0 A) (dfa_run A w) ].
-
-    Lemma f_correct: MH_rel_ref _ (dfa_lang A) f.
-    Proof.
-      move => w1 w2.
-      rewrite /f /=.
-      move => H0 w3.
-      rewrite /dfa_lang /= 2!in_simpl /= -2!dfa_run_accepts 2!dfa_run'_cat 2!last_cat.
-      by rewrite H0.
-    Qed.
-  End ToMN.
-  
   Section ToDFA.
     Variable L: language char.
     Variable X: finType.
@@ -54,7 +40,7 @@ Section MyhillNerode.
       move => x.
       by move: (xchooseP (f_surj x)) => /eqP {2}<-.
     Qed.
-
+    
     Definition repr := [ fun w => f_inv (f w) ].
                                          
     Lemma f_inv_inj: injective f_inv.
@@ -63,6 +49,26 @@ Section MyhillNerode.
       move: (xchooseP (f_surj x)) => /eqP {2}<-.
       move: (xchooseP (f_surj y)) => /eqP {2}<-.
       by rewrite /f_inv => ->.
+    Qed.
+
+    Lemma f_inv_invariant_rcons w a: f (rcons (f_inv (f w)) a) = f (rcons w a).
+    Proof.
+      apply ref.
+      move => z.
+      rewrite -2!cats1 -2!catA /=. pattern (a::z).
+      assert (MH_rel L (f_inv (f w)) w).
+        apply ref.
+        by rewrite f_invK.
+      exact: H.
+    Qed.
+
+    Lemma f_inv_invariant_L w: f_inv (f w) \in L <-> (w \in L).
+    Proof.
+      move: (ref (f_inv (f w)) w) => [H1 H2].
+      rewrite f_invK in H1.
+      move: (H1 (Logic.eq_refl _)) => H3.
+      move: (H3 [::]).
+      by rewrite 2!cats0.
     Qed.
       
     Definition state : finType := X.
@@ -84,23 +90,36 @@ Section MyhillNerode.
       apply: last_ind => [|w a IHw] //.
       rewrite -{1}cats1 dfa_run'_cat last_cat IHw /=.
       rewrite /step.
-      
+      exact: f_inv_invariant_rcons.
+    Qed.
       
     Lemma MN_dfa_correct: L =1 dfa_lang MN_dfa.
     Proof.
-      case/lastP.
-        have H1: f [::] = f (f_inv s0).
-          move: (xchooseP (f_surj s0)) => /eqP.
-          by rewrite /s0 /f_inv.
-        apply/idP/idP => /= H0.
-          rewrite /fin /=.
-          move: (ref _ _ H1 [::]).
-          rewrite 2!cats0.
-          case.
-          by auto.
-        move: (ref _ _ H1 [::]) => /=.
-        rewrite cats0. case => _ H2.
-        apply: H2.
-        
+      move => w.
+      rewrite /dfa_lang /= -dfa_run_accepts MN_dfa_run_f in_simpl /=.
+      apply/idP/idP;
+      by rewrite f_inv_invariant_L.
+    Qed.
+      
   End ToDFA.
+
+  Section ToMN.
+    Variable A: dfa char.
+    Definition f : Fin_eq_cls A := [ fun w => last (dfa_s0 A) (dfa_run A w) ].
+
+    Lemma f_correct: MH_rel_ref _ (dfa_lang A) f.
+    Proof.
+      move => w1 w2.
+      rewrite /f /=.
+      split.
+        move => H0 w3.
+        rewrite /dfa_lang /= 2!in_simpl /= -2!dfa_run_accepts 2!dfa_run'_cat 2!last_cat.
+        by rewrite H0.
+      move => H0.
+      move: (H0 [::]) => [].
+      rewrite 2!cats0 2!in_simpl /dfa_lang /= -2!dfa_run_accepts. 
+      move => H1 H2.
+    Qed.
+  End ToMN.
+  
 End MyhillNerode.
