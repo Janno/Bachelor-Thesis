b10442bb7cf3bf7d9e0b2c160e76b139c34a25f3
Jan-Oliver Kaiser <mail@janno-kaiser.de>
2012-08-14 17:06:25 +0200

    fixed missing fixpoint code extraction

--- docs/extract_definitions.py
+++ docs/extract_definitions.py
@@ -8,7 +8,7 @@ T = stdin.read()
 module = argv[1]
 target = argv[2]
 
-for (D, T, N) in re.findall('((Definition|Inductive)\s+(\S+).*?:=.*?\.)', T, re.S):
+for (D, T, N) in re.findall('((Definition|Inductive|Record|Fixpoint)\s+(\S+).*?:=.*?\.)', T, re.S):
     f = file('%s/%s_%s' % (target, module, N), 'w')
     f.write(D)
     f.close()
