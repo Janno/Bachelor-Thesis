07ab10d9d5a9e7dba143dad88b06db7b5f15f839
Jan-Oliver Kaiser <mail@janno-kaiser.de>
2012-08-30 07:45:57 +0200

    fixed a bug where .+1 could be regarded as the end of a definition

--- docs/extract_definitions.py
+++ docs/extract_definitions.py
@@ -8,12 +8,12 @@ text = stdin.read()
 module = argv[1]
 target = argv[2]
 
-for (D, T, N) in re.findall('((Definition|Inductive|Record|Fixpoint)\s+([^:\s]+).*?:=.*?\.)', text, re.S):
+for (D, T, N) in re.findall('((Definition|Inductive|Record|Fixpoint)\s+([^:\s]+).*?:=.*?\.)[^+]', text, re.S):
     f = file('%s/%s_%s' % (target, module, N), 'w')
     f.write(D)
     f.close()
 
-for (D, H, T, N, S, E) in re.findall('(((Lemma)\s+(\S+).*?:(.*?)\.)\s+Proof\..*?(Qed|Defined)\.)', text, re.S):
+for (D, H, T, N, S, E) in re.findall('(((Lemma)\s+(\S+).*?:(.*?)\.)\s+Proof\..*?(Qed|Defined)\.)[^+]', text, re.S):
     f = file('%s/%s_%s' % (target, module, N), 'w')
     f.write(D)
     f.close()
@@ -21,7 +21,7 @@ for (D, H, T, N, S, E) in re.findall('(((Lemma)\s+(\S+).*?:(.*?)\.)\s+Proof\..*?
     f.write(H)
     f.close()
 
-for (D, _, N, T) in re.findall('((Variable)\s+([^:\s]+)\s*?:(.+?)\.)', text, re.S):
+for (D, _, N, T) in re.findall('((Variable)\s+([^:\s]+)\s*?:(.+?)\.)[^+]', text, re.S):
     f = file('%s/%s_%s' % (target, module, N), 'w')
     f.write(D)
     f.close()
