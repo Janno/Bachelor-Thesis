6074c021fb7340afb2b89a84c781a43512400b10
Jan-Oliver Kaiser <mail@janno-kaiser.de>
2012-10-04 20:32:15 +0200

    removed comments before extracting definitions

--- docs/extract_definitions.py
+++ docs/extract_definitions.py
@@ -3,12 +3,13 @@
 import re
 from sys import stdin, argv
 
-text = stdin.read()
+text1 = stdin.read()
+text = re.sub(r'\(\*(.+?)\*\)',r'',text1, flags=re.S + re.I)
 
 module = argv[1]
 target = argv[2]
 
-for (D, T, N) in re.findall('((Definition|Inductive|Record|Fixpoint)\s+([^:\s]+).*?:=.*?\.)[^+(12]', text, re.S):
+for (D, T, N) in re.findall('((Function|Definition|Inductive|Record|Fixpoint)\s+([^:\s]+).*?:=.*?\.)[^+(12]', text, re.S):
     f = file('%s/%s_%s' % (target, module, N), 'w')
     f.write(D)
     f.close()
